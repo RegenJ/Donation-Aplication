897c9418e646f45edbd0783908ceab05bd921a26
kasia <kasilebioda@gmail.com>
2017-12-16 02:56:00 +0100

    Add Donation View and  Form

---
+++ androidApp/app/src/main/java/com/example/kasia/helpinghand/DonationView.java
@@ -0,0 +1,13 @@
+package com.example.kasia.helpinghand;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DonationView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donation_view);
+    }
+}
